46212a291066ee032312a69ac84900367f307663
Pierre <pierre.forstmann@free.fr>
2022-02-15 18:52:31 +0100

    added steps in test0

--- sql/test0.sql
+++ sql/test0.sql
@@ -1,3 +1,7 @@
+--
+create user postgres superuser;
+--
+\c contrib_regression postgres
 drop extension if exists pg_set_acl;
 --
 create extension pg_set_acl;
@@ -7,8 +11,24 @@ select pgsa_grant('work_mem','u');
 --
 create user test;
 --
+\c contrib_regression test
+set work_mem='1GB';
+--
+\c contrib_regression postgres 
 select pgsa_grant('work_mem','test');
 select * from pg_set_acl;
+--
+\c contrib_regression test
+set work_mem='1GB';
+show work_mem;
+--
+\c contrib_regression postgres
 select pgsa_revoke('work_mem','test');
 --
+\c contrib_regression test
+set work_mem='1GB';
+--
+\c contrib_regression postgres
 drop extension pg_set_acl;
+--
+
